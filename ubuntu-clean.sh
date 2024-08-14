84ab57211c830b31c0bd9cf4b9c8b3e173e99577
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2024-08-14 11:01:13 +0200

    Add files via upload

---
+++ ubuntu-clean.sh
@@ -0,0 +1,29 @@
+apt list --installed > installed.1st
+for pkg in bolt btrfs* containerd cryptsetup* dmeventd dmidecode dmsetup docker.io docker-doc docker-compose docker-compose-v2 lvm2 lxd* mdadm open-iscsi overlayroot podman-docker runc sg3-utils* thin-provisioning* wireless* xfsprogs; do sudo apt purge -y $pkg; done
+apt autoremove --purge -y
+
+---
+exit 0
+apt update
+apt install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt full-upgrade -y
+deluser lxd
+delgroup lxd
+groupadd docker
+useradd -mru 999 -s /bin/bash -g docker docker
+byobu-enable
+reboot
+
+---
+exit 0
+wget https://cloudron.io/cloudron-setup
+chmod +x ./cloudron-setup
+./cloudron-setup
+byobu-enable
+reboot
