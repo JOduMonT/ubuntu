1322aeef29589087b401512c79176ac16b357d97
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-02-08 10:01:31 +0100

    Update cloudron-clean.sh

--- cloudron-clean.sh
+++ cloudron-clean.sh
@@ -2,6 +2,8 @@ apt list --installed > installed.1st
 for pkg in aspell* bolt btrfs* containerd cryptsetup* dmeventd dmidecode dmsetup docker.io docker-doc docker-compose docker-compose-v2 fonts* lvm2 lxd* mdadm nftables* ntfs* open-iscsi overlayroot podman-docker runc snap* sg3-utils* telnet thin-provisioning* tnftp usb-* wamerican wireless* xfsprogs; do sudo apt purge -y $pkg; done
 apt autoremove --purge -y
 
+byobu-enable
+
 wget https://cloudron.io/cloudron-setup
 chmod +x ./cloudron-setup
 ./cloudron-setup
