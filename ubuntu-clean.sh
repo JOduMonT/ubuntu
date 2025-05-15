7b3033534ab1de6a380517f67497ac340b9ceb51
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-05-15 10:01:17 +0200

    Update ubuntu-clean.sh

--- ubuntu-clean.sh
+++ ubuntu-clean.sh
@@ -1,5 +1,3 @@
 apt list --installed > installed.1st
 for pkg in aspell* bolt btrfs* containerd cryptsetup* dmeventd dmidecode dmsetup docker.io docker-doc docker-compose docker-compose-v2 fonts* lvm2 lxd* mdadm nftables* ntfs* open-iscsi overlayroot podman-docker runc snap* sg3-utils* telnet thin-provisioning* tnftp usb-* wamerican wireless* xfsprogs; do sudo apt purge -y $pkg; done
 apt autoremove --purge -y
-byobu-enable
-reboot
