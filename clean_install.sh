a8a791432695a1f8475cb603d8d061f130ac528f
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-06-05 11:06:54 +0200

    Create clean_install.sh

---
+++ clean_install.sh
@@ -0,0 +1,52 @@
+apt list --installed > installed.1st
+apt update
+apt install -y etckeeper
+
+apt purge \
+  apport* \
+  btrfs-progs \
+  containerd \
+  cryptsetup* \
+  dmeventd \
+  dmidecode \
+  dmsetup \
+  doc-base \
+  docker.io \
+  docker-doc \
+  docker-compose \
+  docker-compose-v2 \
+  lvm2 \
+  lxd* \
+  info \
+  man-db \
+  manpages* \
+  mdadm \
+  multipath-tools \
+  open-iscsi \
+  overlayroot \
+  podman-docker \
+  popularity-contest \
+  runc \
+  sg3-utils* \
+  snapd \
+  thin-provisioning-tools \
+  ubuntu-docs \
+  ubuntu-report \
+  xfsprogs \
+  whoopsie
+apt autoremove --purge -y
+
+apt dist-upgrade -y
+
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+reboot
