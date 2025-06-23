9de4f12058ade703ad7f4d3c06ce5f70e2fc1a42
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-06-23 06:27:59 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -9,3 +9,12 @@ echo \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# ADD USER Docker
+useradd -g docker -mNs /bin/bash -u 911 docker
+cp -r /root/.ssh /home/docker/
+chown -R docker:docker /home/docker/.ssh
+
+# ENABLE BYOBU for Docker user
+sudo -nu docker byobu-enable
+sudo -nu docker echo "set -sg escape-time 50" > /home/docker/.config/byobu/.tmux.conf
