2a018fc8524056305aef9abe6052f2da3bcbdf44
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-06-23 06:42:27 +0200

    Create install_nodejs.sh

---
+++ install_nodejs.sh
@@ -0,0 +1,6 @@
+# https://github.com/nodesource/distributions?tab=readme-ov-file#ubuntu-versions
+# choose your version
+## NODE=24
+
+curl -sL https://deb.nodesource.com/setup_24.x | bash -
+apt install -y nodejs
