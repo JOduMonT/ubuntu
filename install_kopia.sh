b5a9a6ae816f6d4b58d4e343560602a6bb30f0e0
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-06-26 12:05:01 +0200

    Create install_kopia.sh

---
+++ install_kopia.sh
@@ -0,0 +1,10 @@
+# ref: https://kopia.io/docs/installation/#linux-installation-using-apt-debian-ubuntu
+curl -s https://kopia.io/signing-key | sudo gpg --dearmor -o /etc/apt/keyrings/kopia-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kopia-keyring.gpg] http://packages.kopia.io/apt/ stable main" | sudo tee /etc/apt/sources.list.d/kopia.list
+sudo apt update
+sudo apt install -y kopia kopia-ui
+
+echo "https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/etc/systemd/system/kopia-server.service"
+
+# config as service: https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/etc/systemd/system/kopia-server.service
+# files to exclude: https://raw.githubusercontent.com/JOduMonT/home-exclusions/refs/heads/master/rsync-homedir-excludes.txt
