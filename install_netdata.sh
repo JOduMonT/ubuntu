3c34052c16349be4449a67a2446e4054ade1de77
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-06-26 14:40:49 +0200

    Create install_netdata.sh

---
+++ install_netdata.sh
@@ -0,0 +1,9 @@
+wget -O /tmp/netdata-kickstart.sh https://get.netdata.cloud/kickstart.sh && sh /tmp/netdata-kickstart.sh --stable-channel --disable-telemetry
+sudo wget -O /etc/netdata/netdata.conf http://localhost:19999/netdata.conf
+
+# edit: /etc/netdata/health_alarm_notify.conf
+# SEND_TELEGRAM="YES"
+# TELEGRAM_BOT_TOKEN="your_bot_token"
+# DEFAULT_RECIPIENT_TELEGRAM="your_chat_id"
+
+# test: /usr/libexec/netdata/plugins.d/alarm-notify.sh test
