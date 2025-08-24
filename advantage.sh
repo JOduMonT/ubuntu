b2524062e05d8e43a733ef87cb29e214042624b9
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-08-24 09:34:41 +0200

    Create advantage.sh

---
+++ advantage.sh
@@ -0,0 +1,4 @@
+# INSTALL ubuntu advantage (Expanded Security Maintenance + Livepatch service)
+## https://ubuntu.com/pro/tutorial
+## https://ubuntu.com/pro/dashboard
+apt install -y ubuntu-advantage-tools
