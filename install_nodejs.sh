5713a91ab04bf2b2039df4a3cbdc373e6a892507
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-08-24 10:15:24 +0200

    Update install_nodejs.sh

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,8 +1,11 @@
+apt update
+apt install -y curl
+
 # https://github.com/nodesource/distributions?tab=readme-ov-file#ubuntu-versions
 # choose your version
 ## NODE=24
 
-curl -sL https://deb.nodesource.com/setup_24.x | bash -
+curl -fsSL https://deb.nodesource.com/setup_24.x | bash -
 apt install -y nodejs
 
 npm install -g npm@latest
