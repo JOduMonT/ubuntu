8de162e7e6d89a72804b2e06937818339adf9109
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-08-24 10:08:09 +0200

    Update install_advantage.sh

--- install_advantage.sh
+++ install_advantage.sh
@@ -1,4 +1,5 @@
 # INSTALL ubuntu advantage (Expanded Security Maintenance + Livepatch service)
 ## https://ubuntu.com/pro/tutorial
 ## https://ubuntu.com/pro/dashboard
+apt update
 apt install -y ubuntu-advantage-tools
