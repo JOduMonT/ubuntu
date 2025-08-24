95377b04cd97c21dd0d1dcdca101ee0172edcd6d
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-08-24 10:10:29 +0200

    Update install_cloudron.sh

--- install_cloudron.sh
+++ install_cloudron.sh
@@ -1,7 +1,5 @@
 apt update
 apt install -y curl
 
-scripts=(install_tools.sh clean_install.sh)
-for script in "${scripts[@]}"; do curl -fsSL "https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/$script"|bash; done
-
+curl -fsSL "https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/clean_install.sh|bash
 curl -fsSL https://cloudron.io/cloudron-setup|bash
