a255a5d765146e8cbbd53eef133960f69e111d25
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-08-30 17:56:12 +0200

    Update install_coolify.sh

--- install_coolify.sh
+++ install_coolify.sh
@@ -2,4 +2,7 @@ apt update
 apt install -y curl
 
 curl -fsSL https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/clean_install.sh|bash
+curl -fsSL https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/install_advantage.sh|bash
+curl -fsSL https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/secure_sshd.sh|bash
+
 curl -fsSL https://cdn.coollabs.io/coolify/install.sh | bash
