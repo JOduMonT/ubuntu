5f7d257ba3a700e3635cfc54262471a0e9e39118
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-09-17 13:12:34 +0200

    Update install_dify.sh

--- install_dify.sh
+++ install_dify.sh
@@ -4,6 +4,7 @@ apt install -y curl jq
 curl -fsSL https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/clean_install.sh|bash
 curl -fsSL https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/install_advantage.sh|bash
 curl -fsSL https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/secure_sshd.sh|bash
+curl -fsSL https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/install_docker.sh|bash
 
 ## https://docs.dify.ai/en/getting-started/install-self-hosted/docker-compose
 git clone --branch "$(curl -s https://api.github.com/repos/langgenius/dify/releases/latest | jq -r .tag_name)" https://github.com/langgenius/dify.git
