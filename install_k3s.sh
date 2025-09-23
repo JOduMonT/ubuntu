67561a9e8b797e901c3d2182a3d42519071aab60
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-09-23 18:28:22 +0200

    Create install_k3s.sh

---
+++ install_k3s.sh
@@ -0,0 +1,8 @@
+apt update
+apt install -y curl
+
+curl -fsSL https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/clean_install.sh|bash
+curl -fsSL https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/install_advantage.sh|bash
+curl -fsSL https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/secure_sshd.sh|bash
+
+curl -sfL https://get.k3s.io | sh -
