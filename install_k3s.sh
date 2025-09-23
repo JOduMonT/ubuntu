3b20395bfd902fdc167b6a89651363377dc9f6e4
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-09-23 21:08:56 +0200

    Update install_k3s.sh

--- install_k3s.sh
+++ install_k3s.sh
@@ -6,3 +6,4 @@ curl -fsSL https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/ins
 curl -fsSL https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/secure_sshd.sh|bash
 
 curl -sfL https://get.k3s.io | sh -
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
