07d7593ab6574f8bcd970599ca4f5a1264662db8
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-09-24 11:46:24 +0200

    Update add_superuser.sh

--- add_superuser.sh
+++ add_superuser.sh
@@ -1,7 +1,11 @@
 # ADD SUPER USER
-useradd -g sudo -mNs /bin/bash $USER
+useradd -g users -mNs /bin/bash $USER
 cp -r /root/.ssh /home/$USER/
 chown -R $USER: /home/$USER/.ssh
+
+usermod -aG sudo $USER
+echo "$USER ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/90-$USER-users"
+
 
 # ENABLE BYOBU
 sudo -nu $USER byobu-enable
