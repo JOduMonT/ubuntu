6183b36d140e5e7046cc3ba4894bfcb1b0979e3d
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-09-24 11:21:00 +0200

    Update install_microk8s.sh

--- install_microk8s.sh
+++ install_microk8s.sh
@@ -6,3 +6,7 @@ curl -fsSL https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/ins
 curl -fsSL https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/secure_sshd.sh|bash
 
 sudo snap install microk8s --classic
+
+# ADD USER Docker
+USER=rescue
+curl -fsSL "https://raw.githubusercontent.com/JOduMonT/ubuntu/refs/heads/main/add_superuser.sh"|bash
