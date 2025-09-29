eb8dd6573afa2a732558c3fadf33fb64a6fa514d
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-09-29 12:21:27 +0200

    Create install_etckeeper.sh

---
+++ install_etckeeper.sh
@@ -0,0 +1,14 @@
+# ref: https://wiki.archlinux.org/title/Etckeeper#Automatic_push_to_remote_repo
+# Pushing your etckeeper repository to a publicly accessible remote repository can expose sensitive data such as password hashes or private keys. Proceed with caution.
+REPO="" # Use a Private Repo
+USERNAME="" # Your GitHub username
+PASSWORD="" # [Use a Classic Token as Password](https://github.com/settings/tokens)
+
+apt install -y etckeeper
+sed -i 's/^PUSH_REMOTE=""/PUSH_REMOTE="origin"/g' /etc/etckeeper/etckeeper.conf
+
+exit 0
+
+cd /etc
+git remove add origin ${REPO}
+git push -u origin main
