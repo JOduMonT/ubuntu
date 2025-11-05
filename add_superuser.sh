a1ddf2131cf281c0a5890931deee1faa1db52213
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-11-05 15:08:54 +0100

    Refactor add_superuser.sh to use default USER

--- add_superuser.sh
+++ add_superuser.sh
@@ -1,12 +1,13 @@
 # ADD SUPER USER
-useradd -g users -mNs /bin/bash $USER
-cp -r /root/.ssh /home/$USER/
-chown -R $USER: /home/$USER/.ssh
+USER=
+useradd -g users -mNs /bin/bash ${USER:-rescue}
+cp -r /root/.ssh /home/${USER:-rescue}/
+chown -R ${USER:-rescue}: /home/${USER:-rescue}/.ssh
 
-usermod -aG sudo $USER
-echo "$USER ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/90-$USER-users"
+usermod -aG sudo ${USER:-rescue}
+echo "${USER:-rescue} ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/90-${USER:-rescue}-users"
 
 
 # ENABLE BYOBU
-sudo -nu $USER byobu-enable
-sudo -nu $USER echo "set -sg escape-time 50" > /home/$USER/.config/byobu/.tmux.conf
+sudo -nu ${USER:-rescue} byobu-enable
+sudo -nu ${USER:-rescue} echo "set -sg escape-time 50" > /home/${USER:-rescue}/.config/byobu/.tmux.conf
