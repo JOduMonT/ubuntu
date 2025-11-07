a945db902b63020c39a60a68e69bb1f49378df85
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2025-11-07 16:05:26 +0100

    Add installation script for yt-dlp

---
+++ install_yt-dlp.sh
@@ -0,0 +1,4 @@
+## https://github.com/yt-dlp/yt-dlp/wiki/Installation
+sudo add-apt-repository -y ppa:tomtomtom/yt-dlp    # Add ppa repo to apt
+sudo apt update                                 # Update package list
+sudo apt install -y yt-dlp                         # Install yt-dlp
