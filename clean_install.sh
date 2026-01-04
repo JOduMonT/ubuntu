da591c703444c3a04ee0ced24573ed7d353999fc
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2026-01-04 09:25:10 +0100

    Add package updates and installation to script

--- clean_install.sh
+++ clean_install.sh
@@ -1,3 +1,5 @@
+apt update
+apt install -y etckeeper
 packages=(apport* btrfs-progs containerd cryptsetup* dmeventd dmidecode dmsetup doc-base docker.io docker-doc docker-compose docker-compose-v2 lvm2 lxd* info man-db manpages* mdadm multipath-tools open-iscsi overlayroot podman-docker popularity-contest python3-babel python3-bcrypt python3-botocore python3-jmespath python3-pip-whl python3-s3transfer runc sg3-utils* snapd thin-provisioning-tools ubuntu-docs ubuntu-report xfsprogs whoopsie)
 for package in "${packages[@]}"; do apt purge -y "$package"; done
 apt autoremove --purge -y
