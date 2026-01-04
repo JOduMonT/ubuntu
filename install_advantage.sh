d7f307206ec38aa91c94db8712d48c258690169c
Jonathan DUMONT <5204724+JOduMonT@users.noreply.github.com>
2026-01-04 09:33:23 +0100

    Update install_advantage.sh with usage comments
    Added comments for Ubuntu Pro client usage and FIPS enabling.

--- install_advantage.sh
+++ install_advantage.sh
@@ -5,6 +5,10 @@ apt update
 apt install -y ubuntu-advantage-tools
 apt dist-upgrade -y
 
+
+## https://ubuntu.com/pro/dashboard
+# pro attach ....
+## then ...
 ## https://ubuntu.com/tutorials/using-the-ubuntu-pro-client-to-enable-fips#4-enabling-fips-crypto-modules
 # pro enable fips-updates
 # pro enable usg
