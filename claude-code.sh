790b907fb649fee87d29e0993e49792e9507bc51
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2026-05-17 13:34:35 +0200

    Create claude-code.sh

---
+++ claude-code.sh
@@ -0,0 +1,10 @@
+# as user
+### https://github.com/nvm-sh/nvm#installing-and-updating
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+npm install -g npm
+
+### https://code.claude.com/docs/en/setup#install-with-npm
+npm install -g @anthropic-ai/claude-code
