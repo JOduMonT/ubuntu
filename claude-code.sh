ad09ce67af4ed8c1282096e19c1713ade4e3fc0b
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2026-05-17 14:00:24 +0200

    Update claude-code.sh

--- claude-code.sh
+++ claude-code.sh
@@ -4,6 +4,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 24
 npm install -g npm
 
 ### https://code.claude.com/docs/en/setup#install-with-npm
